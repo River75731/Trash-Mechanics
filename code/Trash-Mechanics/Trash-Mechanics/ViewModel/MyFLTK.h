93980baaab59c1d8e5db5e4cd38377862d3e5723
River75731 <279360503@qq.com>
2019-07-10 09:11:27 +0800

    Remove unnecessary images & create viewmodel template

---
+++ code/Trash-Mechanics/Trash-Mechanics/ViewModel/MyFLTK.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Double_Window.H>
+#include <string>
+
+class MyPoint 
+{
+private:
+	int m_X;
+	int m_Y;
+};
--- code/Trash-Mechanics/Trash-Mechanics/main.cpp
+++ code/Trash-Mechanics/Trash-Mechanics/main.cpp
@@ -1,16 +1,24 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <Fl_PNG_Image.H>
+#include <Fl_Shared_Image.H>
 #include <iostream>
 #include <vector>
 #include "Common.h"
 #include "View-Polygon.h"
-
+using namespace std;
 int main(int argc, char *argv[])
 {
-	Fl_Window  *window;
-	window = new  Fl_Window(450, 330, "First App!");
-	window->end();
-	window->show(argc, argv);
-	return  Fl::run();
+		fl_register_images();
+		Fl_Window     win(720, 486);
+		Fl_Box        box(10, 10, 11, 11);
+		Fl_Box box2(100, 100, 101, 101);
+		Fl_PNG_Image  png(".//Images//1.png");
+		box.image(png);
+		box2.image(png);
+		win.show();
+		box2.hide();
+		win.redraw();
+		return(Fl::run());
 }
