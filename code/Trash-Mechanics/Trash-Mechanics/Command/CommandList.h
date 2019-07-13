4146e223e980bc8e7e15d07f7c4ccf5eb8d71f25
angrypop <waangrypop@gmail.com>
2019-07-13 10:55:31 +0800

    Test

--- code/Trash-Mechanics/Trash-Mechanics/Command/CommandList.h
+++ code/Trash-Mechanics/Trash-Mechanics/Command/CommandList.h
@@ -2,3 +2,4 @@
 //the file should include all the Command Real Body headfile
 #include "../Command/CreateRigidBodyDataCommand.h"
 #include "../Command/CreatePolyViewCommand.h"
+#include "../Command/SimulateTimeFlyDataCommand.h"
